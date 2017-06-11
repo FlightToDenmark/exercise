a108c730029924e2de6eb8fa4cc34e2fed6d8642
FlightToDenmark <shabity@outlook.com>
2017-06-11 12:42:30 +0900

    Create Routing.java

--- Routing.java
+++ Routing.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,56 +10,39 @@ import java.util.StringTokenizer;
 public class Routing {
 
 	public static int vertex, edge;
-	public static BigDecimal[] distance;
-	public static PriorityQueue<BigPair> queue;
-	public static List<List<BigPair>> adjacent;
-	
-	public static class BigPair implements Comparable<BigPair> {
-
-		int destNode; BigDecimal dist;
-
-		public BigPair(int a, BigDecimal b) {
-			this.destNode = a;
-			this.dist = b;
-		}
-
-		@Override
-		public int compareTo(BigPair pair) {
-			
-			return this.dist.compareTo(pair.dist);
-		
-		}
-			
-	}
+	public static double[] distance;
+	public static PriorityQueue<Pair> queue;
+	public static List<List<Pair>> adjacent;
 
 	public static void main(String[] args) {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		try {
 			int testCase = Integer.parseInt(br.readLine());
 
-			while (testCase--> 0) {
+			while (testCase-- > 0) {
 
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				vertex = Integer.parseInt(st.nextToken());
 				edge = Integer.parseInt(st.nextToken());
-				distance = new BigDecimal[vertex];
-				Arrays.fill(distance, new BigDecimal("99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"));
-				adjacent = new ArrayList<List<BigPair>>();
-				for (int i = 0; i < vertex; i++) adjacent.add(new ArrayList<BigPair>());
+				distance = new double[vertex];
+				Arrays.fill(distance, -1);
+				adjacent = new ArrayList<List<Pair>>();
+				for (int i = 0; i < vertex; i++)
+					adjacent.add(new ArrayList<Pair>());
 				queue = new PriorityQueue<>(); // Pair<v, dist[v]>
 
 				for (int e = 0; e < edge; e++) {
 					StringTokenizer s = new StringTokenizer(br.readLine());
 					int from = Integer.parseInt(s.nextToken());
 					int to = Integer.parseInt(s.nextToken());
-					BigDecimal cost = new BigDecimal(Double.parseDouble(s.nextToken()));
-					adjacent.get(from).add(new BigPair(to, cost));
-					adjacent.get(to).add(new BigPair(from, cost));
+					double cost = Double.parseDouble(s.nextToken());
+					adjacent.get(from).add(new Pair(to, cost));
+					adjacent.get(to).add(new Pair(from, cost));
 				}
 
-				System.out.format("%.10f\n", dijkstra(0));
+				System.out.format("%.11f\n", dijkstra(0));
 
 			}
 
@@ -70,24 +52,24 @@ public class Routing {
 
 	}
 
-	public static BigDecimal dijkstra(int src) {
+	public static double dijkstra(int src) {
 
-		distance[src] = BigDecimal.ONE;
-		queue.add(new BigPair(src, BigDecimal.ONE));
+		distance[src] = 1;
+		queue.add(new Pair(src, 1));
 
 		while (!queue.isEmpty()) {
-			BigPair pair = queue.poll();
-			BigDecimal dist = pair.dist;
+			Pair pair = queue.poll();
+			double dist = pair.dist;
 			int here = pair.destNode;
 			
-			if (distance[here].compareTo(dist)<0) continue;
-			
+			if (distance[here] < dist && distance[here] > 0)
+				continue;
 			for (int i = 0; i < adjacent.get(here).size(); i++) {
 				int there = adjacent.get(here).get(i).destNode;
-				BigDecimal nextDist = dist.multiply(adjacent.get(here).get(i).dist);
-				if (distance[there].compareTo(nextDist)>0) {
+				double nextDist = dist * adjacent.get(here).get(i).dist;
+				if (distance[there] > nextDist || distance[there] < 0) {
 					distance[there] = nextDist;
-					queue.add(new BigPair(there, nextDist));
+					queue.add(new Pair(there, nextDist));
 				}
 			}
 
@@ -97,4 +79,23 @@ public class Routing {
 
 	}
 
+}
+
+class Pair implements Comparable<Pair> {
+
+	int destNode; double dist;
+
+	public Pair(int a, double b) {
+		super();
+		this.destNode = a;
+		this.dist = b;
+	}
+
+	@Override
+	public int compareTo(Pair pair) {
+		
+		return (int)(this.dist - pair.dist);
+	
+	}	
+	
 }
